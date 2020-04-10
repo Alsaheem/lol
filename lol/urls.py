8294c605e457e15285743495484cadcb1f73e640
Adebisi Ayomide <usiwoma.oghenovo@rosabon-finance.net>
2020-04-10 18:06:14 +0100

    cleanup

--- lol/urls.py
+++ lol/urls.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
 from django.urls import path,include
-<<<<<<< HEAD
-from django.conf import settings
-from django.conf.urls.static import static
-# from django.conf.urls import handler404, handler500
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('', include('todo.urls', namespace='notes')),
-=======
 from lol import views
 
 
@@ -21,5 +11,4 @@ urlpatterns = [
     path('accounts/login/', views.login_view, name='login'),
     path('accounts/logout/', views.logout_view, name='logout'),
     path('accounts/register/', views.register_view, name='register'),
->>>>>>> develop
 ]
